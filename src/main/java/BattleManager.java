0251c41a729d96e0d8d26399c233dc8432c0f1ec
gjmooney <gjmooney@asu.edu>
2021-11-16 17:44:17 +0100

    Add BattleManager class

---
+++ src/main/java/BattleManager.java
@@ -0,0 +1,62 @@
+package main.java;
+
+import attacks.java.TalentDrainAttack;
+import attacks.java.TradeSecretTheftAttack;
+import attacks.java.UndercutPriceAttack;
+import players.java.StartUp;
+
+import java.util.Scanner;
+
+public class BattleManager {
+    static Scanner in = new Scanner(System.in);
+    static int turn;
+
+    public void startBattle(StartUp player1, StartUp player2) {
+        // player 1 goes first
+        turn = 1;
+        int choice = 0;
+        boolean fightOver = false;
+
+        // Prompt for attack choice
+        do {
+            System.out.println("Choose attack: ");
+            System.out.println("1) Talent Drain"
+            + "\n2) Trade Secret Theft"
+            + "\n3) Undercut Prices");
+
+            choice = in.nextInt();
+            //choice = 1;
+
+            int damage = calcAttack(player1, choice);
+            player2.getAttacked(damage);
+
+            if (damage < Constants.MINIMUM_DAMAGE_DONE) {
+                damage = Constants.MINIMUM_DAMAGE_DONE;
+            }
+
+            // if hp < 0 fight is over
+
+        } while (!fightOver);
+    }
+
+    public int calcAttack(StartUp player, int attack) {
+
+        switch (attack) {
+            case 1:
+                player.setAttackStrategy(new TalentDrainAttack());
+                break;
+            case 2:
+                player.setAttackStrategy(new TradeSecretTheftAttack());
+                break;
+            case 3:
+                player.setAttackStrategy(new UndercutPriceAttack());
+                break;
+            default:
+                System.out.println("Not an attack");
+        }
+
+        return player.doAttack();
+    }
+
+
+}
