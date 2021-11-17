033cc989eed8cc59f9a642652064536679f184a4
gjmooney <gjmooney@asu.edu>
2021-11-17 19:03:43 +0100

    Test battle logic

--- src/main/java/silicon_sim/Main.java
+++ src/main/java/silicon_sim/Main.java
@@ -1,48 +1,58 @@
 package main.java.silicon_sim;
 
 import main.java.silicon_sim.players.*;
-import main.java.silicon_sim.Cycle;
 
 public class Main {
     public static void main(String[] args) {
         System.out.println("Setup");
+        GameLogic logic = GameLogic.getLogic();
 
-        //Set up player 1
+        //Create tech giants
         TechGiant evilInc = new SocialMediaTechGiant("Evil Inc.");
-        StartUp willow = new RealEstateStartUp("Willow");
-        evilInc.addStartUp(willow);
-        TechGiant.techGiants.add(evilInc);
-
-        //Set up player 2
         TechGiant tepid = new OsTechGiant("Tepid");
-        StartUp markHealth = new HealthCareStartUp("MarkHealth");
-        tepid.addStartUp(markHealth);
-        TechGiant.techGiants.add(tepid);
+        logic.addTechGiant(evilInc);
+        logic.addTechGiant(tepid);
 
-        //Wild start ups
+        //Create startups
+        StartUp willow = new RealEstateStartUp("Willow");
+        StartUp markHealth = new HealthCareStartUp("MarkHealth");
         StartUp greyRock = new FinTechStartUp("GreyRock");
         StartUp xulin = new SocialMediaStartUp("Xulin");
+        logic.addStartUp(willow);
+        logic.addStartUp(markHealth);
+        logic.addStartUp(greyRock);
+        logic.addStartUp(xulin);
+
+        //Tech giants acquire startups
+        evilInc.addStartUp(willow);
+        tepid.addStartUp(markHealth);
 
         //Set up game
         Cycle.setQuarter(1);
 
-        System.out.println("Evil");
+        /*System.out.println("Evil");
         for (StartUp su : evilInc.getStartUps()) {
-            System.out.println(su.getRevenue());
-            System.out.println(su.getOwner());
+            System.out.println(su.getHealth());
+            System.out.println(su.getOwnerName());
         }
 
         System.out.println("Tepid");
         for (StartUp su : tepid.getStartUps()) {
-            System.out.println(su.getRevenue());
-            System.out.println(su.getOwner());
+            System.out.println(su.getHealth());
+            System.out.println(su.getOwnerName());
+        }
+
+        for (StartUp su : GameLogic.getAllStartUps()) {
+            System.out.println(su.getName() + " : "+ su.getHealth());
         }
 
         QuarterEvents.taxCuts();
 
-        for (StartUp su : evilInc.getStartUps()) {
-            System.out.println(su.getRevenue());
-        }
+        for (StartUp su : GameLogic.getAllStartUps()) {
+            System.out.println(su.getName() + " : "+ su.getHealth());
+        }*/
+
+        logic.getBattleManager().startBattle(willow, markHealth);
 
     }
 }
