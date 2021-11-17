f129e97ce3c6d13787e575763f876fac211b15b0
gjmooney <gjmooney@asu.edu>
2021-11-17 19:00:49 +0100

    Update constants

--- src/main/java/silicon_sim/Constants.java
+++ src/main/java/silicon_sim/Constants.java
@@ -3,4 +3,6 @@ package main.java.silicon_sim;
 public class Constants {
     public final static Double SAME_TYPE_ATTACK_BONUS = 1.2;
     public final static int MINIMUM_DAMAGE_DONE = 1;
+    public final static int BASE_EXPERIENCE = 10;
+    public final static int MAX_LEVEL = 10;
 }
